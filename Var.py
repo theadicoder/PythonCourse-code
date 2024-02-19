895d56c07a5654df96ae0f3856c39a1f80c6e7b0
ADITYA THAKUR <Aadicorian@outlook.com>
2024-02-19 13:46:09 +0000

    updated code

--- Var.py
+++ Var.py
@@ -1,3 +1,16 @@
-name = "Aditya Thakur"
-age = 15
-print((name),(age))
+# Variable Example
+
+# name = "Aditya Thakur"
+# age = 15
+
+# name = "Aman"
+# age = 24
+
+# is_adult = False
+# print(name,age)
+
+# Exercise 
+
+name = "Tony"
+age = 51
+is_genius = True
